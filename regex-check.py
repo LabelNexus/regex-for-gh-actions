4bd8793f312ab7f76244c27eda2a9d4ad4d398ca
gnorred88 <gnorred88@gmail.com>
2020-01-27 20:10:49 +0000

    Some cleanup

--- regex-check.py
+++ regex-check.py
@@ -1,35 +1,23 @@
-from os import getenv, environ, listdir
+from os import getenv, environ
 from re import findall
 from sys import exit
 import json
 
-print(environ, flush=True)
-
-workspace = getenv('GITHUB_WORKSPACE')
 event_path = getenv('GITHUB_EVENT_PATH')
 
-print([f for f in listdir('/github/workflow')], flush=True)
-
-with open('/github/workflow/event.json') as f:
+with open(event_path) as f:
   event_data = json.load(f)
-  print(event_data.keys(), flush=True)
 
-print(event_data['pull_request'].keys(), flush=True)
-print(event_data.get('pull_request', {}).get('title'), flush=True)
-
-try:
-  regex = getenv('INPUT_PATTERN')
-except:
-  exit('You must provide a pattern using the "with:" key in the workflow config')
-
-try:
-  string = getenv('INPUT_STRING')
-except:
-  exit('You must provide a string using the "with:" key in the workflow config')
+regex = getenv('INPUT_PATTERN')
+string = getenv('INPUT_STRING')
 
 string = str(string)
 match = findall(regex, string)
+
 print(dir(match), flush=True)
+if match:
+  for x in match:
+    print(x, flush=True)
 
 if match:
   print("The regex found a match", flush=True)
