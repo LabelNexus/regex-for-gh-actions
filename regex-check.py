3cccbac77bf15f97d94835c2bb7557b341eb3d1c
gnorred88 <gnorred88@gmail.com>
2020-01-27 21:33:17 +0000

    Rm outputs for now

--- regex-check.py
+++ regex-check.py
@@ -1,7 +1,6 @@
-from os import getenv, environ
 from re import findall
+from os import getenv
 from sys import exit
-import json
 
 regex = getenv('INPUT_PATTERN')
 string = getenv('INPUT_STRING')
@@ -12,7 +11,6 @@ match = findall(regex, string)
 if match:
   print(f"The regex found a match in the provided string: {string}", flush=True)
   print(f"Matche(s): {match}", flush=True)
-  environ['OUTPUT_MATCHES'] = str(match)
   exit()
 
 exit(f"No match was found for the provided pattern - {regex} - and string - {string}")
